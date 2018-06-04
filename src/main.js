36395235c0b0bd35061dd3a1833753cc79d71ce3
zjq41981 <zjq41981@ly.com>
2018-06-04 16:02:55 +0800

    单选

--- src/main.js
+++ src/main.js
@@ -8,8 +8,9 @@ import 'element-ui/lib/theme-chalk/index.css';
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import iView from 'iview';
 import 'iview/dist/styles/iview.css';
+import _ from 'lodash'
 
-
+Vue.prototype._ = _
 Vue.use(VueAwesomeSwiper)//轮播
 Vue.use(iView);
 Vue.config.productionTip = false
